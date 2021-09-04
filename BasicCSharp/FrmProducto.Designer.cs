48345f1d8831db5b3539c2425e152212476066ec
dara0212 <90067132+dara0212@users.noreply.github.com>
2021-09-04 13:56:40 -0600

    Agregar archivos de proyecto.

---
+++ BasicCSharp/FrmProducto.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace BasicCSharp
+{
+    partial class FrmProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpCadu = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPre = new System.Windows.Forms.TextBox();
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(275, 2);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(58, 29);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Aceptar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.BtnMostrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Codigo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.dtpCadu);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPre);
+            this.groupBox1.Controls.Add(this.txtCant);
+            this.groupBox1.Controls.Add(this.txtDesc);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtCod);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(335, 271);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos producto";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 241);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Caducidad:";
+            // 
+            // dtpCadu
+            // 
+            this.dtpCadu.Location = new System.Drawing.Point(74, 237);
+            this.dtpCadu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpCadu.Name = "dtpCadu";
+            this.dtpCadu.Size = new System.Drawing.Size(258, 20);
+            this.dtpCadu.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 207);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Precio:";
+            // 
+            // txtPre
+            // 
+            this.txtPre.Location = new System.Drawing.Point(74, 205);
+            this.txtPre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPre.Name = "txtPre";
+            this.txtPre.Size = new System.Drawing.Size(258, 20);
+            this.txtPre.TabIndex = 9;
+            // 
+            // txtCant
+            // 
+            this.txtCant.Location = new System.Drawing.Point(74, 169);
+            this.txtCant.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(258, 20);
+            this.txtCant.TabIndex = 8;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(74, 103);
+            this.txtDesc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(258, 50);
+            this.txtDesc.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(74, 69);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(258, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(74, 35);
+            this.txtCod.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(258, 20);
+            this.txtCod.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 171);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Cantidad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Descripcion:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnMostrar);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 286);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(335, 32);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // FrmProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 391);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmProducto";
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpCadu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPre;
+        private System.Windows.Forms.TextBox txtCant;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ Infraestructure/Producto/ProductoModel.cs
@@ -0,0 +1,96 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure
+{
+    public class ProductoModel
+    {
+        private Producto[] productos;
+
+        public void agregar(Producto pe)
+        {
+            if(pe == null)
+            {
+                throw new ArgumentException("Error, el producto no puede ser null.");
+            }
+
+            if(productos == null)
+            {
+                productos = new Producto[1];
+                productos[productos.Length - 1] = pe;
+                return;
+            }
+
+            Producto[] tmp = new Producto[productos.Length + 1];
+            Array.Copy(productos, tmp, productos.Length);
+            tmp[tmp.Length - 1] = pe;
+            productos = tmp;
+        }
+
+        public bool aactualizar (Producto pe)
+        {
+            bool success = false;
+            int index = GetIndex(pe);
+            if(index < 0)
+            {
+                throw new ArgumentException($"Error, producto con codigo {pe.Codigo} no existe.");
+            }
+
+            productos[index] = pe;
+            return !success;
+        }
+
+        public bool Delete(Producto p)
+        {
+            bool flag = false;
+            int index = GetIndex(p);
+            if(index < 0)
+            {
+                throw new ArgumentException($"Error, producto con codigo {p.Codigo} no existe.");
+            }
+            Producto[] tmp = new Producto[productos.Length - 1];
+            productos[index] = productos[productos.Length - 1]; 
+            Array.Copy(productos, tmp, productos.Length - 1);
+            productos = tmp;
+            
+            return !flag;
+        }
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+
+        public Producto FindById(int id)
+        {
+            int index = -1;
+            for(int i = 0; i < productos.Length; i++)
+            {
+                if(productos[i].Codigo == id)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            
+            return index < 0 ? null : productos[index];
+        }
+
+        public int GetIndex(Producto p)
+        {
+            int index = -1, i = 0;
+            foreach(Producto prod in productos)
+            {
+                if(prod.Codigo == p.Codigo)
+                {
+                    index = i;
+                    break;
+                }
+                i++;
+            }
+
+            return index;
+        }
+    }
+}
